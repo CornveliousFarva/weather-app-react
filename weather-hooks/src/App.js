107b00e5dbfe1fc9fe9f9f2b3b00d443d24d9d28
Greg Schuman <gcschuman@hotmail.com>
2020-10-24 15:58:28 -0700

    Added error handling.

--- weather-hooks/src/App.js
+++ weather-hooks/src/App.js
@@ -14,17 +14,26 @@ function App() {
     const apiData = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${APIKEY}`)
       .then( res => res.json())
       .then(data => data)
-      setWeather({
-        data: apiData,
-        city: apiData.city,
-        country: apiData.sys.country,
-        description: apiData.weather[0].description,
-        temperature: apiData.main.temp,
-        error: ""
-      }
-      )
-  }
-
+      if(city && country) {
+        setWeather({
+          data: apiData,
+          city: apiData.city,
+          country: apiData.sys.country,
+          description: apiData.weather[0].description,
+          temperature: apiData.main.temp,
+          error:""
+        }
+        )} else {
+          setWeather({
+            data: '',
+            city: '',
+            country: '',
+            description: '',
+            temperature: '',
+            error:"Please Type A City And Country"
+        }
+        )}
+    }
   return (
     <div className="App">
       <h3>WEATHER APP</h3>
